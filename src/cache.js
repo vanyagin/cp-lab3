e3eece88285a17babcc43e311d12388a2dac7345
vanyagin <58004940+vanyagin@users.noreply.github.com>
2022-09-27 13:46:15 +0300

    1

--- src/cache.js
+++ src/cache.js
@@ -1,4 +1,26 @@
 class Cache{
-
+    constructor() {
+        this.cache = {}
+    }
+    add(key, value, access = 1){
+        this.cache[key] = [value, access]
+    }
+    get(key){
+        if (this.cache[key][0] == null || this.cache[key][0] == undefined || this.cache[key][1] < 1)
+            return null
+        else{
+            this.cache[key][1] -= 1
+            return this.cache[key]
+        }
+    }
+    stat(key){
+        return `key: ${key}; value: ${this.cache[key][0]}; access: ${this.cache[key][1]}`
+    }
+    statistic(){
+        const res = []
+        for(let x in this.cache)
+            res.push(this.stat(x))
+        return res
+    }
 }
 export {Cache}
--- test/cache.test.js
+++ test/cache.test.js
@@ -1,5 +1,32 @@
 import {Cache} from "../src/cache";
+const cache = new Cache()
 
-test('it fails', () => {
-    expect(false).toBe(true);
+test('stat test', () => {
+    const c = new Cache();
+    c.add("c", "[c, 100]", 100)
+    expect(c.stat("c")).toBe("key: c; value: [c, 100]; access: 100");
 });
+
+test('statistic test', () => {
+    const c = new Cache();
+    c.add("c", "[c, 100]", 100)
+    c.add("c2", "[c2, 100]", 100)
+    expect(c.statistic()[0]).toBe("key: c; value: [c, 100]; access: 100");
+    expect(c.statistic()[1]).toBe("key: c2; value: [c2, 100]; access: 100");
+});
+
+test('get test', () => {
+    const c = new Cache();
+    c.add("c", "[c, 100]", 100)
+    c.add("c2", "[c2, 100]", 100)
+    c.add("c0", "null", 1)
+    c.add("n", null, 1337)
+    c.add("n0")
+    expect(c.get("c")[0]).toBe("[c, 100]");
+    expect(c.get("c")[1]).toBe(98);
+    expect(c.get("c0")[0]).toBe("null");
+    expect(c.get("c0")).toBe(null);
+    expect(c.get("n")).toBe(null);
+    expect(c.get("n0")).toBe(null);
+});
+
